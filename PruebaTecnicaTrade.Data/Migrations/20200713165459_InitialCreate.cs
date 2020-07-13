4b84278c09a99c537ce9320d8a0b09e142306091
davidstiven123 <davidstiven123@gmail.com>
2020-07-13 12:03:46 -0500

    EntityFrameWork Creado

---
+++ PruebaTecnicaTrade.Data/Migrations/20200713165459_InitialCreate.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PruebaTecnicaTrade.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tiendas",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Nombre = table.Column<string>(nullable: false),
+                    Fecha = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tiendas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Productos",
+                columns: table => new
+                {
+                    SKU = table.Column<Guid>(nullable: false),
+                    TiendaId = table.Column<Guid>(nullable: false),
+                    Nombre = table.Column<string>(nullable: false),
+                    Descripcion = table.Column<string>(nullable: false),
+                    Valor = table.Column<decimal>(nullable: false),
+                    Fecha = table.Column<DateTime>(nullable: false),
+                    Imagen = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productos", x => x.SKU);
+                    table.ForeignKey(
+                        name: "FK_Productos_Tiendas_TiendaId",
+                        column: x => x.TiendaId,
+                        principalTable: "Tiendas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Productos_TiendaId",
+                table: "Productos",
+                column: "TiendaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Productos");
+
+            migrationBuilder.DropTable(
+                name: "Tiendas");
+        }
+    }
+}
---
+++ PruebaTecnicaTrade.Data/PruebaTecnicaTradeContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PruebaTecnicaTrade.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PruebaTecnicaTrade.Data
+{
+    public class PruebaTecnicaTradeContext : DbContext
+    {
+        //public PruebaTecnicaTradeContext(DbContextOptions<PruebaTecnicaTradeContext> options) : base(options)
+        //{
+
+        //}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+             //"Data Source=SQL5046.site4now.net;" +
+             //"Initial Catalog=DB_A4FD02_prueba1234567;User Id=DB_A4FD02_prueba1234567_admin;Password=d_link2010.;");
+             "Data Source=.; Initial Catalog=DB_PrebatecnicaTrade; Integrated Security=True;");
+            //"Data Source=sql5032.site4now.net;" +
+            //    "Initial Catalog=DB_A53E0A_ConfeccionesPrueba;User Id=DB_A53E0A_ConfeccionPrueba_admin;Password=d-link2010.;");
+            base.OnConfiguring(optionsBuilder);
+            //Primer Migracion   Add-Migration InitialCreate
+
+            //Despues de la primera migracion se utiliza  Update-Database
+        }
+        public DbSet<Tienda> Tiendas { get; set; }
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
