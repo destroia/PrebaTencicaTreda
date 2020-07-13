bf9661b8729d2f6285ca85078c2d08051e91cdd2
davidstiven123 <davidstiven123@gmail.com>
2020-07-13 12:51:49 -0500

    Creacion de metodos de DB

---
+++ PruebaTecnicaTrade.Data/Interfazes/IProductosRepository.cs
@@ -0,0 +1,16 @@
+﻿using PruebaTecnicaTrade.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaTecnicaTrade.Data.Interfazes
+{
+    public interface IProductosRepository
+    {
+        Task<bool> DeleteProducto(Guid id);
+        Task<Producto> PostCreateProducto(Producto Producto);
+        Task<bool> PostUpdateProducto(Producto Producto);
+        Task<IEnumerable<Producto>> GetProductos();
+    }
+}
---
+++ PruebaTecnicaTrade.Data/Repository/ProductosRepository.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PruebaTecnicaTrade.Data.Interfazes;
+using PruebaTecnicaTrade.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaTecnicaTrade.Data.Repository
+{
+    public class ProductosRepository : IProductosRepository
+    {
+        PruebaTecnicaTradeContext DB = new PruebaTecnicaTradeContext();
+
+        public async Task<bool> DeleteProducto(Guid id)
+        {
+            try
+            {
+                var P = await DB.Productos.FindAsync(id);
+                DB.Productos.Remove(P);
+                await DB.SaveChangesAsync();
+
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public async Task<IEnumerable<Producto>> GetProductos()
+        {
+            try
+            {
+                var LP = await DB.Productos.ToListAsync();
+
+                return LP;
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
+            
+        }
+
+        public async Task<Producto> PostCreateProducto(Producto Producto)
+        {
+            try
+            {
+                await DB.Productos.AddAsync(Producto);
+                await DB.SaveChangesAsync();
+                return Producto;
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
+        }
+
+        public async Task<bool> PostUpdateProducto(Producto Producto)
+        {
+           
+            try
+            {
+                DB.Productos.Update(Producto);
+                await DB.SaveChangesAsync();
+
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+    }
+}
